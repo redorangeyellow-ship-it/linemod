e53c34ad258c6e7dfa5c4c7333dceda6b814117a
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-07-18 00:24:17 -0700

    Update addon-manifests.js

--- src/addons/generated/addon-manifests.js
+++ src/addons/generated/addon-manifests.js
@@ -74,6 +74,7 @@ import _tw_remove_backpack from "../addons/tw-remove-backpack/_manifest_entry.js
 import _tw_disable_cloud_variables from "../addons/tw-disable-cloud-variables/_manifest_entry.js";
 import _multi_tab_code from "../addons/multi-tab-code/_manifest_entry.js";
 import _editor_animations from "../addons/editor-animations/_manifest_entry.js";
+import _reorder_custom_inputs from "../addons/reorder-custom-inputs/_manifest_entry.js";
 
 export default {
   "cat-blocks": _cat_blocks,
@@ -152,4 +153,5 @@ export default {
   "waveform-chunk-size": _waveform_chunk_size,
   "multi-tab-code": _multi_tab_code,
   "editor-animations": _editor_animations,
+  "reorder-custom-inputs": _reorder_custom_inputs,
 };
