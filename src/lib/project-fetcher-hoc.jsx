5c108bfe9f26749db0ea911bd947ac718fd8190d
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-01 00:48:23 -0700

    maybe i was right the first time

--- src/lib/project-fetcher-hoc.jsx
+++ src/lib/project-fetcher-hoc.jsx
@@ -115,7 +115,11 @@ const ProjectFetcherHOC = function (WrappedComponent) {
             this.props.vm.stop();
 
             // pm: clear url params when fetching if the project ID is 'default'
-            if (loadingState === LoadingStates.FETCHING_NEW_DEFAULT && (projectId == 0 || projectId === null)) {
+            console.log(loadingState);
+            if (
+                (loadingState === LoadingStates.LOADING_VM_NEW_DEFAULT || loadingState === LoadingStates.FETCHING_NEW_DEFAULT)
+                && (projectId == 0 || projectId === null)
+            ) {
                 const path = window.location.pathname + window.location.hash;
                 window.history.replaceState({}, document.title, path);
                 this.props.vm.setFramerate(30);
