576e144f5921dcfba4be37dac616030ee6148f91
jwklong <72522395+jwklong@users.noreply.github.com>
2025-08-05 09:34:48 +0100

    hide move up down block

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -30,13 +30,6 @@ const motion = function (isInitialSetup, isStage, targetId) {
                 </shadow>
             </value>
         </block>
-        <block type="motion_moveupdownsteps">
-            <value name="STEPS">
-                <shadow type="math_number">
-                    <field name="NUM">10</field>
-                </shadow>
-            </value>
-        </block>
         <block type="motion_turnright">
             <value name="DEGREES">
                 <shadow type="math_number">
