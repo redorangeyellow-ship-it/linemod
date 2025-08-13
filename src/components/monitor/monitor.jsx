2217a5b0685810c542d0a87afa2a98e3186609c1
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-12 22:55:56 -0700

    put ID here too

--- src/components/monitor/monitor.jsx
+++ src/components/monitor/monitor.jsx
@@ -37,7 +37,7 @@ const MonitorComponent = props => (
         // TW: if export is defined, we always show it, even outside of the editor
         disable={!props.draggable && !props.onExport}
         holdToDisplay={props.mode === 'slider' ? -1 : 1000}
-        id={`monitor-${props.label}`}
+        id={`monitor-${props.id}`}
     >
         <Draggable
             bounds=".monitor-overlay" // Class for monitor container
