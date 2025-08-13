c4af8e338a955a16432a9116fcfb7e99dff04278
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-08-12 22:50:33 -0700

    monitor.jsx -- try using id instead of label

--- src/components/monitor/monitor.jsx
+++ src/components/monitor/monitor.jsx
@@ -66,7 +66,7 @@ const MonitorComponent = props => (
             // positioning conflicts between the monitors `transform: scale` and
             // the context menus `position: fixed`. For more details, see
             // http://meyerweb.com/eric/thoughts/2011/09/12/un-fixing-fixed-elements-with-css-transforms/
-            <ContextMenu id={`monitor-${props.label}`}>
+            <ContextMenu id={`monitor-${props.id}`}>
                 {props.draggable && props.onSetModeToDefault &&
                     <MenuItem onClick={props.onSetModeToDefault}>
                         <FormattedMessage
