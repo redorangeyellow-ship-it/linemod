9e0e516de8eb073f5b4b5b06d525e0275cb90003
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-09-02 00:58:15 -0700

    Update make-toolbox-xml.js

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -1465,6 +1465,7 @@ const liveTests = function () {
         </block>
         ${blockSeparator}
         <block type="control_fieldbutton"></block>
+        <block type="control_expandableCase"></block>
         <block type="motion_mutatorCheckboxTest"></block>
         ${blockSeparator}
         <block type="control_dualblock"></block>
