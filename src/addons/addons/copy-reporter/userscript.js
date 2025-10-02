c0105c6e4ed87520bbf03e42686560c3f44b4195
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-10-01 21:22:31 -0700

    fix the addon code to work with pm bubbles

--- src/addons/addons/copy-reporter/userscript.js
+++ src/addons/addons/copy-reporter/userscript.js
@@ -20,7 +20,59 @@ export default async function ({ addon, console, msg }) {
   // add button to reporter bubble
   const ScratchBlocks = await addon.tab.traps.getBlockly();
 
-  // https://github.com/scratchfoundation/scratch-blocks/blob/893c7e7ad5bfb416eaed75d9a1c93bdce84e36ab/core/workspace_svg.js#L979
+  // https://github.com/PenguinMod/PenguinMod-Blocks/blob/develop/core/workspace_svg.js#L1017C1
+  ScratchBlocks.WorkspaceSvg.prototype.reportValue = function(id, value, isError) {
+    var block = this.getBlockById(id);
+    if (!block) {
+      throw 'Tried to report value on block that does not exist.';
+    }
+    ScratchBlocks.DropDownDiv.hideWithoutAnimation();
+    ScratchBlocks.DropDownDiv.clearContent();
+    var contentDiv = ScratchBlocks.DropDownDiv.getContentDiv();
+
+    var valueReportBox = goog.dom.createElement('div');
+    valueReportBox.setAttribute('class', 'valueReportBox');
+    if (typeof value !== 'object' || typeof value.toReporterContent !== 'function') {
+      valueReportBox.textContent = String(value);
+    } else {
+      valueReportBox.appendChild(value.toReporterContent());
+    }
+
+    if (!addon.self.disabled) {
+      // use to get focus and event priority
+      valueReportBox.setAttribute("tabindex", "0");
+      // if the user pressed Ctrl+C, prevent propagation to Blockly
+      valueReportBox.onkeydown = (event) => {
+        if ((event.altKey || event.ctrlKey || event.metaKey) && event.code === "KeyC") {
+          event.stopPropagation();
+        }
+      };
+
+      if (value.length !== 0) {
+        const copyButton = document.createElement("img");
+        copyButton.setAttribute("role", "button");
+        copyButton.setAttribute("tabindex", "0");
+        copyButton.setAttribute("alt", msg("copy-to-clipboard"));
+        copyButton.setAttribute("src", addon.self.getResource("/copy.svg")) /* rewritten by pull.js */;
+
+        copyButton.classList.add("sa-copy-reporter-icon");
+        addon.tab.displayNoneWhileDisabled(copyButton);
+
+        copyButton.onclick = () => navigator.clipboard.writeText(value);
+        valueReportBox.append(document.createElement("br"), copyButton);
+      }
+    }
+
+    contentDiv.appendChild(valueReportBox);
+    ScratchBlocks.DropDownDiv.setColour(
+      ScratchBlocks.Colours.valueReportBackground,
+      isError 
+        ? ScratchBlocks.Colours.blockError 
+        : ScratchBlocks.Colours.valueReportBorder
+    );
+    ScratchBlocks.DropDownDiv.showPositionedByBlock(this, block);
+  };
+  
   ScratchBlocks.WorkspaceSvg.prototype.reportValue = function (id, value) {
     let block = this.getBlockById(id);
     if (!block) {
