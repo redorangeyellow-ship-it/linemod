7b92dc298009876da894e086b08afcf64abf84f6
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-10-08 20:43:06 -0700

    shit negative

--- src/addons/addons/custom-block-shape/userscript.js
+++ src/addons/addons/custom-block-shape/userscript.js
@@ -229,7 +229,7 @@ export default async function ({ addon, console }) {
         l 0 -2
         a ${GRID_UNIT * multiplier} ${GRID_UNIT * multiplier} 0 0 0 ${-GRID_UNIT * multiplier} ${-GRID_UNIT * multiplier}
         a ${GRID_UNIT * multiplier} ${GRID_UNIT * multiplier} 0 0 1 ${-GRID_UNIT * multiplier} ${-GRID_UNIT * multiplier}
-        l 0 ${4 * (multiplier * multiplier)}
+        l 0 ${-4 * (multiplier * multiplier)}
         a ${GRID_UNIT * multiplier} ${GRID_UNIT * multiplier} 0 0 1 ${GRID_UNIT * multiplier} ${-GRID_UNIT * multiplier}
         a ${GRID_UNIT * multiplier} ${GRID_UNIT * multiplier} 0 0 0 ${GRID_UNIT * multiplier} ${-GRID_UNIT * multiplier}
         l 0 -2
