1848315859d833a5b8f89f0fe1afb10f3497b104
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-10-28 14:05:09 -0700

    Clone Count Addon -- DO NOT patch runtime.step. Use proper events

--- src/addons/addons/clones/userscript.js
+++ src/addons/addons/clones/userscript.js
@@ -67,24 +67,10 @@ export default async function ({ addon, console, msg }) {
     doCloneChecks(true);
   });
 
-  const oldStep = vm.runtime._step;
-  vm.runtime._step = function (...args) {
-    const ret = oldStep.call(this, ...args);
-    doCloneChecks();
-    return ret;
-  };
-
-  /*
-  if (addon.self.enabledLate) {
-    // Clone count might be inaccurate if the user deleted sprites
-    // before enabling the addon
-    let count = 0;
-    for (let target of vm.runtime.targets) {
-      if (!target.isOriginal) ++count;
-    }
-    vm.runtime._cloneCounter = count;
-  }
-  */
+  vm.runtime.on("targetWasCreated", (_, originalTarget) => {
+    if (originalTarget) doCloneChecks();
+  });
+  vm.runtime.on("targetWasRemoved", doCloneChecks);
 
   while (true) {
     await addon.tab.waitForElement('[class*="controls_controls-container"]', {
