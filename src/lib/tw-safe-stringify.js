73f8a60ef4da6d5a1b74dcbb1d6999b90b1803aa
RedMan13 <godslayerakp@gmail.com>
2026-04-03 16:55:17 -0700

    ok, but WHY the FUCK does this list content EVER become string

--- src/lib/tw-safe-stringify.js
+++ src/lib/tw-safe-stringify.js
@@ -2,46 +2,46 @@
  * Since PenguinMod has custom monitor displays,
  * we have to handle that too when sanitizing.
  */
-const isCustomMonitor = (value) => {
-  if (
-    typeof (value.toListItem || value.toMonitorContent || value.toReporterContent) === 'function'
-  ) {
-    return true;
-  } else {
-    return false
-  }
-}
-
-const circularReplacer = () => {
-  const seen = new WeakSet();
-  return (_, value) => {
-    if (typeof value === 'object' && value !== null) {
-      if (seen.has(value)) {
-        return Array.isArray(value) ? '[...]' : '{...}';
-      }
-      seen.add(value);
+const isCustomMonitor = value => {
+    if (
+        typeof (value.toListItem || value.toMonitorContent || value.toReporterContent) === 'function'
+    ) {
+        return true;
     }
-    return value;
-  };
+    return false;
+    
 };
 
-const sanitize = (input) => {
-  if (
-    typeof input === "object" && input !== null
+const circularReplacer = () => {
+    const seen = new WeakSet();
+    return (_, value) => {
+        if (typeof value === 'object' && value !== null) {
+            if (seen.has(value)) {
+                return Array.isArray(value) ? '[...]' : '{...}';
+            }
+            seen.add(value);
+        }
+        return value;
+    };
+};
+
+const sanitize = input => {
+    if (
+        typeof input === 'object' && input !== null
     && !isCustomMonitor(input)
-  ) {
-    return JSON.stringify(input, circularReplacer());
-  } else {
+    ) {
+        return JSON.stringify(input, circularReplacer());
+    }
     return input;
-  }
+    
 };
 
 const sanitizeVariableType = (input, type) => {
-  if (type === "list") {
-    return input.map(item => sanitize(item));
-  } else {
+    if (type === 'list' && Array.isArray(input)) {
+        return input.map(item => sanitize(item));
+    }
     return sanitize(input);
-  }
+  
 };
 
 export { sanitizeVariableType };
