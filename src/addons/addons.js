285f1cb52f81d6183b27f8eb670b35fd1d0122b9
obeyorbehacked750-jpg <obeyorbehacked750@gmail.com>
2026-08-13 21:33:03 +0300

    Update addons.js

--- src/addons/addons.js
+++ src/addons/addons.js
@@ -81,7 +81,8 @@ const addons = [
     'tw-disable-vibration',
     'tw-disable-cloud-variables',
     'tw-disable-compiler',
-    'editor-stepping'
+    'editor-stepping',
+    'improvements'
 ];
 
 const newAddons = [
