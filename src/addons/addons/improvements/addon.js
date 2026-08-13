cfd8f66d5fc9c1781aabb512bf79db96ad693033
obeyorbehacked750-jpg <obeyorbehacked750@gmail.com>
2026-08-13 21:49:33 +0300

    Update and rename addon.json to addon.js

---
+++ src/addons/addons/improvements/addon.js
@@ -0,0 +1,17 @@
+const manifest = {
+  "editorOnly": true,
+  "noTranslations": true,
+  "noCompiler": true,
+  "name": "Improvements...",
+  "description": "Some chipywarp improvements like rounded menus and some uttilites...",
+  "dynamicDisable": true,
+  "userscripts": [
+    {
+      "url": "userscript.js"
+    }
+  ],
+  "settings": [],
+  "tags": [],
+  "enabledByDefault": false
+};
+export default manifest;
