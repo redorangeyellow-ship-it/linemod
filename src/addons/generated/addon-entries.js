7837108450396b0bfca0778343b43e7aae5dfaa0
obeyorbehacked750-jpg <obeyorbehacked750@gmail.com>
2026-08-14 12:49:14 +0300

    Add import for improvements addon entry

--- src/addons/generated/addon-entries.js
+++ src/addons/generated/addon-entries.js
@@ -81,4 +81,5 @@ export default {
   "tw-disable-cloud-variables": () => import(/* webpackChunkName: "addon-entry-tw-disable-cloud-variables" */ "../addons/tw-disable-cloud-variables/_runtime_entry.js"),
   "tw-disable-compiler": () => import(/* webpackChunkName: "addon-entry-tw-disable-compiler" */ "../addons/tw-disable-compiler/_runtime_entry.js"),
   "editor-stepping": () => import(/* webpackChunkName: "addon-entry-editor-stepping" */ "../addons/editor-stepping/_runtime_entry.js"),
+  "improvements": () => import(/* webpackChunkName: "addon-entry-editor-stepping" */ "../addons/improvements/_runtime_entry.js"),
 };
