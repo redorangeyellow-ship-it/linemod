9b61888f86da178061066b09b032630eec2a13ac
redorangeyellow-ship-it <darkred95@gmail.com>
2026-09-01 12:12:15 -0400

    Enhance JSDoc in monitor-adapter.js
    Corrected JSDoc comment formatting and improved clarity of parameter descriptions.

--- src/lib/monitor-adapter.js
+++ src/lib/monitor-adapter.js
@@ -2,19 +2,17 @@ import OpcodeLabels from './opcode-labels.js';
 import {safeStringify} from './tw-safe-stringify.js';
 
 const isUndefined = a => typeof a === 'undefined';
-
-/**
+/
  * Convert monitors from VM format to what the GUI needs to render.
  * - Convert opcode to a label and a category
  * @param {string} block.id - The id of the monitor block
  * @param {string} block.spriteName - Present only if the monitor applies only to the sprite
- *     with given target ID. The name of the target sprite when the monitor was created
+    with given target ID. The name of the target sprite when the monitor was created
  * @param {string} block.opcode - The opcode of the monitor
  * @param {object} block.params - Extra params to the monitor block
  * @param {string|number|Array} block.value - The monitor value
  * @param {VirtualMachine} block.vm - the VM instance which owns the block
  * @return {object} The adapted monitor with label and category
- */
 export default function ({id, mode, spriteName, opcode, params, value, vm}) {
     // Extension monitors get their labels from the Runtime through `getLabelForOpcode`.
     // Other monitors' labels are hard-coded in `OpcodeLabels`.
