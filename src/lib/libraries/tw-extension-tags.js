d65a52f0ee7a10116377354587a44ce08eb3ace0
redorangeyellow-ship-it <darkred95@gmail.com>
2026-09-02 18:41:04 -0400

    Remove comment about translatability of brand names
    Removed unnecessary comment about brand names being non-translatable.

--- src/lib/libraries/tw-extension-tags.js
+++ src/lib/libraries/tw-extension-tags.js
@@ -1,5 +1,5 @@
-// Because there are all brand names, it is unnecessary for them to be translatable.
 export default [
     {tag: 'scratch', intlLabel: 'Scratch'},
     {tag: 'tw', intlLabel: 'TurboWarp'}
+    {tag: 'penguinmod', intlLabel: 'PenguinMod'}
 ];
